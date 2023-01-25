57211d853a4f758f55aea39f8154989116600a52
Samuela <115798007+koffi-samuela@users.noreply.github.com>
2023-01-25 09:54:24 +0100

    modif

--- script.py
+++ script.py
@@ -1,2 +1,6 @@
-print("hello world")
-print("toto")
+# On utilise donc la fonction sorted avec en paramètre la liste des données (clé + valeurs) de l'objet dict (la fonction items() de l'objet dict) et en clé de tri, une fonction lambda indiquant l'indice à utiliser pour le tri.
+# reverse = true pour l'orde décroissant
+# L'indice '0' (zéro) indique la clé de l'objet dict.
+# Pour un tri suivant les valeurs, il suffit d'indiquer l'indice '1' (un)
+# >>> sorted(d1.items(), key=lambda t: t[1])
+[('a', 1), ('b', 2), ('c', 3), ('d', 4), ('a1', 5), ('d1', 6), ('c1', 8), ('b1', 10)]
